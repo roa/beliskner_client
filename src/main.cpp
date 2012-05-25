5b26012367d70bbec7dca663fdb19a1787779414
Robert Abraham <r.abraham@mueller-wulff.com>
2012-05-25 19:08:29 +0200

    initial

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    try
+    {
+        //app creation
+    }
+    catch( ... )
+    {
+        std::cerr << "An exception has occured " << std::endl;
+    }
+
+    return 0;
+}
